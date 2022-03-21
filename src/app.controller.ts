cefbec792c63b2038a2c79c2df81c04e2471e4ad
David <db9949@student.uni-lj.si>
2022-03-21 08:35:43 +0100

    Reformat with es-lint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Param } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("routeToLocation/:bbox")
+  getRouteToLocation(@Param() bbox: string) {
+    return this.appService.getRouteToLocation(bbox);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import fetch from "node-fetch";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
+  }
+
+  // https://wiki.openstreetmap.org/wiki/API_v0.6#Retrieving_map_data_by_bounding_box:_GET_.2Fapi.2F0.6.2Fmap
+  async getRouteToLocation(bbox: string) {
+    let data;
+    try {
+      data = await fetch(
+        `https://wiki.openstreetmap.org/api/0.6/map?bbox=${bbox}`
+      );
+    } catch (ex) {
+      console.error(ex);
+    }
+
+    return data;
   }
 }
